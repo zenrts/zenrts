8f798c58716845065476dd69075d95bd7955ed61
Ian Torres <iantorres@outlook.com>
2026-07-06 10:54:41 -0400

    fix: add hang detection with 120s timeout + backtrace to find Strand deadlock

--- benchmarks/benchmark_core.cpp
+++ benchmarks/benchmark_core.cpp
@@ -15,6 +15,49 @@
 
 #include <benchmark/benchmark.h>
 #include <zenrts/core.h>
+#include <cstdlib>
+#include <csignal>
+#include <iostream>
+#include <execinfo.h>
+#include <dlfcn.h>
+#include <cxxabi.h>
+#include <cstring>
+#include <unistd.h>
+
+static void hang_handler(int sig)
+{
+    std::cerr << "\n=== BENCHMARK TIMEOUT (60s) ===\n";
+    std::cerr << "Backtrace of current thread (tid=" << gettid() << "):\n";
+
+    void* buffer[128];
+    int frames = backtrace(buffer, 128);
+
+    for (int i = 0; i < frames; ++i) {
+        Dl_info info;
+        if (dladdr(buffer[i], &info)) {
+            int status = 0;
+            char* demangled = abi::__cxa_demangle(info.dli_sname, nullptr, nullptr, &status);
+            const char* name = demangled ? demangled : info.dli_sname;
+            std::cerr << "  #" << i << " " << name << " +" << (uintptr_t)buffer[i] - (uintptr_t)info.dli_saddr << "\n";
+            free(demangled);
+        } else {
+            std::cerr << "  #" << i << " <unknown>\n";
+        }
+    }
+
+    std::cerr << "===============================\n";
+    abort();
+}
+
+static void setup_hang_detection()
+{
+    struct sigaction sa;
+    std::memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = hang_handler;
+    sa.sa_flags = SA_RESTART | SA_NODEFER;
+    sigaction(SIGALRM, &sa, nullptr);
+    alarm(120);
+}
 
 static void BM_Version(benchmark::State& state)
 {
@@ -25,3 +68,13 @@ static void BM_Version(benchmark::State& state)
     }
 }
 BENCHMARK(BM_Version);
+
+int main(int argc, char** argv)
+{
+    setup_hang_detection();
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
+    alarm(0);
+    return 0;
+}
